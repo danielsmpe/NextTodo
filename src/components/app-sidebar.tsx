2d1823f377a79a2f804acdb131cfee74e2f0dee8
Daniel Sampe <danielsampe61@gmail.com>
2025-06-07 16:52:02 +0800

    feat: add sidebar component and integrate shadcn/ui

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,63 @@
+'use client';
+
+import * as React from 'react';
+import { BookOpen, Frame, Map, PieChart, Settings, SquareTerminal } from 'lucide-react';
+
+import { NavMain } from '@/components/nav-main';
+import { NavProjects } from '@/components/nav-projects';
+import { Sidebar, SidebarContent } from '@/components/ui/sidebar';
+
+// This is sample data.
+const data = {
+  user: {
+    name: 'shadcn',
+    email: 'm@example.com',
+    avatar: '/avatars/shadcn.jpg',
+  },
+  navMain: [
+    {
+      title: 'Dashboard',
+      url: '#',
+      icon: SquareTerminal,
+      isActive: true,
+    },
+    {
+      title: 'Calender',
+      url: '#',
+      icon: BookOpen,
+    },
+    {
+      title: 'Settings',
+      url: '#',
+      icon: Settings,
+    },
+  ],
+  projects: [
+    {
+      name: 'Design Engineering',
+      url: '#',
+      icon: Frame,
+    },
+    {
+      name: 'Sales & Marketing',
+      url: '#',
+      icon: PieChart,
+    },
+    {
+      name: 'Travel',
+      url: '#',
+      icon: Map,
+    },
+  ],
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+        <NavProjects projects={data.projects} />
+      </SidebarContent>
+    </Sidebar>
+  );
+}
